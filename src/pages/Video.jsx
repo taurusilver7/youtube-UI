1eee657a226ef16ddc9fc357d09058d5faef129a
taurusilver7 <prasad20697@gmail.com>
2022-07-17 00:39:11 +0530

    video page created

--- src/pages/Video.jsx
+++ src/pages/Video.jsx
@@ -1,7 +1,166 @@
 import React from "react";
+import styled from "styled-components";
+import ThumbUpOutlinedIcon from "@mui/icons-material/ThumbUpOutlined";
+import ThumbDownOutlinedIcon from "@mui/icons-material/ThumbDownOutlined";
+import ReplyOutlinedIcon from "@mui/icons-material/ReplyOutlined";
+import AddTaskOutlinedIcon from "@mui/icons-material/AddTaskOutlined";
+import MoreHorizOutlinedIcon from "@mui/icons-material/MoreHorizOutlined";
+import Comments from "../components/Comments";
+import CardAlt from "../components/CardAlt";
+
+const Container = styled.div`
+  display: flex;
+  gap: 24px;
+`;
+const Content = styled.div`
+  flex: 5;
+`;
+const VideoWrapper = styled.div``;
+const Title = styled.h1`
+  font-size: 18px;
+  font-weight: 400;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  color: ${({ theme }) => theme.text};
+`;
+const Details = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+const Info = styled.span`
+  font-size: 14px;
+  color: ${({ theme }) => theme.textSoft};
+`;
+
+const Hr = styled.hr`
+  margin: 10px 0px;
+  border: 0.5px solid ${({ theme }) => theme.soft};
+`;
+
+const Buttons = styled.div`
+  display: flex;
+  gap: 20px;
+`;
+const Button = styled.button`
+  display: flex;
+  align-items: center;
+  color: ${({ theme }) => theme.textSoft};
+  background-color: transparent;
+  border: none;
+  gap: 5px;
+`;
+const Recommendation = styled.div`
+  flex: 2;
+`;
+const Channel = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+const ChannelInfo = styled.div`
+  display: flex;
+  gap: 20px;
+`;
+
+const Image = styled.img`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+`;
+
+const ChannelDetail = styled.div`
+  display: flex;
+  flex-direction: column;
+  color: ${({ theme }) => theme.text};
+`;
+
+const ChannelName = styled.span`
+  font-weight: 500;
+`;
+
+const ChannelCounter = styled.span`
+  margin-top: 5px;
+  margin-bottom: 20px;
+  color: ${({ theme }) => theme.textSoft};
+  font-size: 12px;
+`;
+
+const Description = styled.p`
+  font-size: 12px;
+`;
+
+const Subscribe = styled.button`
+  background-color: #cc1a00;
+  font-weight: 500;
+  color: white;
+  border: none;
+  border-radius: 3px;
+  height: max-content;
+  padding: 10px 20px;
+  cursor: pointer;
+`;
 
 const Video = () => {
-  return <div>Video Page</div>;
+  return (
+    <Container>
+      <Content>
+        <VideoWrapper>
+          <iframe
+            width="100%"
+            height="450"
+            src="https://www.youtube.com/embed/k3Vfj-e1Ma4"
+            title="Youtube Video Player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+        </VideoWrapper>
+        <Title>Video Name</Title>
+        <Details>
+          <Info>views & date</Info>
+          <Buttons>
+            <Button>
+              <ThumbUpOutlinedIcon /> 112
+            </Button>
+            <Button>
+              <ThumbDownOutlinedIcon /> 80
+            </Button>
+            <Button>
+              <ReplyOutlinedIcon /> Share
+            </Button>
+            <Button>
+              <AddTaskOutlinedIcon /> Save
+            </Button>
+            <Button>
+              <MoreHorizOutlinedIcon />
+            </Button>
+          </Buttons>
+        </Details>
+        <Hr />
+        <Channel>
+          <ChannelInfo>
+            <Image src="https://yt3.ggpht.com/yti/APfAmoE-Q0ZLJ4vk3vqmV4Kwp0sbrjxLyB8Q4ZgNsiRH=s88-c-k-c0x00ffffff-no-rj-mo" />
+            <ChannelDetail>
+              <ChannelName>Channel Name</ChannelName>
+              <ChannelCounter>200K subscribers</ChannelCounter>
+              <Description>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Doloribus laborum delectus unde quaerat dolore
+              </Description>
+            </ChannelDetail>
+          </ChannelInfo>
+          <Subscribe>SUBSCRIBE</Subscribe>
+        </Channel>
+        <Hr />
+        <Comments />
+      </Content>
+      <Recommendation>
+        <CardAlt />
+        <CardAlt />
+        <CardAlt />
+      </Recommendation>
+    </Container>
+  );
 };
 
 export default Video;
