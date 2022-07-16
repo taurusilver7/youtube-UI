afc87fdad34aa25d21d0c694c0028b40a638e89a
taurusilver7 <prasad20697@gmail.com>
2022-07-16 18:06:54 +0530

    theme init

--- src/App.js
+++ src/App.js
@@ -1,27 +1,38 @@
-import styled from "styled-components";
+import styled, { ThemeProvider } from "styled-components";
 import Menu from "./components/Menu";
 import Navbar from "./components/Navbar";
+import { DarkMode } from "./utlis/Theme";
 
 const Container = styled.div`
-  background-color: #282c34;
+  background-color: ${({ theme }) => theme.bg};
   min-height: 100vh;
   display: flex;
   align-items: center;
-  ${"" /* justify-content: center; */}
+  /* justify-content: center; */
   color: white;
 `;
 const Main = styled.div`
   flex: 7;
+  background-color: #181818;
 `;
+const Wrapper = styled.div``;
 
 function App() {
   return (
-    <Container>
-      <Menu />
-      <Main>
-        <Navbar />
-      </Main>
-    </Container>
+    <ThemeProvider theme={DarkMode}>
+      <Container>
+        <Menu />
+        <Main>
+          <Navbar />
+          <Wrapper>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+          </Wrapper>
+        </Main>
+      </Container>
+    </ThemeProvider>
   );
 }
 
