0661e12b6fc851c9bd03f36c0d4b623db8d523c0
taurusilver7 <prasad20697@gmail.com>
2022-07-16 18:06:41 +0530

    menu component styled

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,15 +1,144 @@
 import React from "react";
 import styled from "styled-components";
 
+import HomeIcon from "@mui/icons-material/Home";
+import ExploreOutlinedIcon from "@mui/icons-material/ExploreOutlined";
+import SubscriptionsOutlinedIcon from "@mui/icons-material/SubscriptionsOutlined";
+import VideoLibraryOutlinedIcon from "@mui/icons-material/VideoLibraryOutlined";
+import HistoryOutlinedIcon from "@mui/icons-material/HistoryOutlined";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+
+import LiveTvOutlinedIcon from "@mui/icons-material/LiveTvOutlined";
+import SportsBasketballOutlinedIcon from "@mui/icons-material/SportsBasketballOutlined";
+import SportsEsportsOutlinedIcon from "@mui/icons-material/SportsEsportsOutlined";
+import LibraryMusicOutlinedIcon from "@mui/icons-material/LibraryMusicOutlined";
+
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
+import FlagOutlinedIcon from "@mui/icons-material/FlagOutlined";
+
 const Container = styled.div`
+  position: sticky;
+  top: 0;
   flex: 1;
-  background-color: #202020;
+  background-color: ${({ theme }) => theme.bg};
   height: 100vh;
-  color: white;
+  color: ${({ theme }) => theme.text};
+  font-size: 15px;
+`;
+
+const Wrapper = styled.div`
+  padding: 1rem 2rem;
+`;
+
+const Logo = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  margin-bottom: 1.5rem;
+`;
+
+const Img = styled.img`
+  height: 25px;
+`;
+const Item = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  cursor: pointer;
+  padding: 0.5rem 0;
+`;
+
+const Hr = styled.hr`
+  margin: 10px 0px;
+  border: 0.5px solid ${({ theme }) => theme.soft};
+`;
+
+const Login = styled.div`
+  font-size: 12px;
+`;
+const Button = styled.button`
+  padding: 0.45rem 0.7rem;
+  background-color: transparent;
+  display: flex;
+  align-items: center;
+  gap: 5;
+  border: 1px solid #3ea6ff;
+  color: #3ea6ff;
+  border-radius: 3px;
+  font-weight: 500;
+  margin-top: 0.7rem;
+  cursor: pointer;
 `;
 
 const Menu = () => {
-  return <Container>Menu Component</Container>;
+  return (
+    <Container>
+      <Wrapper>
+        <Logo>
+          <Img src="logo.png" alt=""></Img>
+          Inshot Premiere
+        </Logo>
+        <Item>
+          <HomeIcon />
+          Home
+        </Item>
+        <Item>
+          <ExploreOutlinedIcon />
+          Explore
+        </Item>
+        <Item>
+          <SubscriptionsOutlinedIcon />
+          Subscriptions
+        </Item>
+        <Hr />
+        <Login>
+          Sign in to like, comment & subscribe
+          <Button>
+            <AccountCircleOutlinedIcon />
+            Sign in
+          </Button>
+        </Login>
+        <Item>
+          <VideoLibraryOutlinedIcon />
+          Library
+        </Item>
+        <Item>
+          <HistoryOutlinedIcon />
+          History
+        </Item>
+        <Item>
+          <LibraryMusicOutlinedIcon />
+          Music
+        </Item>
+        <Item>
+          <SportsEsportsOutlinedIcon />
+          Gaming
+        </Item>
+        <Item>
+          <SportsBasketballOutlinedIcon />
+          Sports
+        </Item>
+        <Hr />
+        <Item>
+          <LiveTvOutlinedIcon />
+          Live
+        </Item>
+        <Item>
+          <SettingsOutlinedIcon />
+          Settings
+        </Item>
+        <Item>
+          <FlagOutlinedIcon />
+          Report
+        </Item>
+        <Item>
+          <HelpOutlineOutlinedIcon />
+          Help
+        </Item>
+      </Wrapper>
+    </Container>
+  );
 };
 
 export default Menu;
