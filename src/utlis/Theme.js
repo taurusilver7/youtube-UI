08971caf5a6c57b2933cd380c751ecec54bb4c0a
taurusilver7 <prasad20697@gmail.com>
2022-07-16 18:06:25 +0530

    theme modes

---
+++ src/utlis/Theme.js
@@ -0,0 +1,14 @@
+export const DarkMode = {
+  bg: "#181818",
+  bgLight: "#202020",
+  text: "white",
+  textSoft: "#aaaaaa",
+  soft: "#373737",
+};
+export const LightMode = {
+  bg: "#f9f9f9",
+  bgLight: "white",
+  text: "black",
+  textSoft: "#606060",
+  soft: "#f5f5f5",
+};
