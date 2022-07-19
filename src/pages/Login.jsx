a601badd3a638691c2dee89104a2265a60f13c17
taurusilver7 <prasad20697@gmail.com>
2022-07-19 14:11:19 +0530

    login page persisted

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import axios from "axios";
+import { useDispatch } from "react-redux";
+import { loginFailure, loginStart, loginSuccess } from "../redux/userSlice";
 
 const Container = styled.div`
   display: flex;
@@ -65,12 +67,18 @@ const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const dispatch = useDispatch()
+
   const handleLogin = async (e) => {
     e.preventDefault();
+    dispatch(loginStart())
     try {
       const res = await axios.post("/auth/signin", { name, password });
       console.log(res.data);
-    } catch (error) {}
+      dispatch(loginSuccess(res.data));
+    } catch (error) {
+      dispatch(loginFailure(error))
+    }
   };
   return (
     <Container>
