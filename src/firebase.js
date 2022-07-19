9539922ba0ac6e18fc9f20b842bb7311c1e48753
taurusilver7 <prasad20697@gmail.com>
2022-07-19 14:13:59 +0530

    firebase config init

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDWUJZIeaZ_UFIUELIJY2jSJIObyBEJqvU",
+  authDomain: "inshot-premiere.firebaseapp.com",
+  projectId: "inshot-premiere",
+  storageBucket: "inshot-premiere.appspot.com",
+  messagingSenderId: "32978171488",
+  appId: "1:32978171488:web:ba1f0fd7a121e57bf7f505",
+  measurementId: "G-P00WP0KT5N",
+};
